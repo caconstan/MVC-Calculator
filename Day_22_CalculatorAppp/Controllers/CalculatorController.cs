5aee7d18c465c698fca2e027ba887233c7cb5c5b
chris <caconstan@yahoo.com>
2020-05-19 14:32:46 -0400

    multi operator updates

---
+++ Day_22_CalculatorAppp/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Day_22_CalculatorAppp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Day_22_CalculatorAppp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        //when you build a new Controller, the models aren't automatically brought over,
+        //so you need to include a using namespaceNameblahblah.Models statement
+        public IActionResult Calculate(Calculator calculator)
+        {
+            if(calculator.Operator == '+')
+            {
+                calculator.Result = calculator.Number1 + calculator.Number2;
+            } 
+            else if (calculator.Operator == '-')
+            {
+                calculator.Result = calculator.Number1 - calculator.Number2;
+            } 
+            else if (calculator.Operator == '*')
+            {
+                calculator.Result = calculator.Number1 * calculator.Number2;
+            }
+            else if (calculator.Operator == '/')
+            {
+                calculator.Result = calculator.Number1 / calculator.Number2;
+            }
+
+            return View("Calculator", calculator);
+        }
+    }
+}
---
+++ Day_22_CalculatorAppp/Models/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day_22_CalculatorAppp.Models
+{
+    public class Calculator
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double Result { get; set; }
+        public char Operator { get; set; }
+
+    }
+}
